77a20d97d1e9d11654be15e3af4f10bc7149a745
Smith <samspike46@gmail.com>
2024-07-01 14:13:02 +0100

    V1.0

--- controllers/apicontroller.php
+++ controllers/apicontroller.php
@@ -9,7 +9,8 @@ class apiController
     }
     public function hello()
     {
-        $visitor_name = trim(@$_GET['visitor_name'], "\"' \t\n\r\0\x0B") ?? "Guest";
+        $visitor_name = trim(@$_GET['visitor_name'], "\"' \t\n\r\0\x0B");
+        $visitor_name = empty($visitor_name) ? "Guest" : $visitor_name;
         $ip = htmlspecialchars($_SERVER['REMOTE_ADDR']);
 
         $weather_api_key = "6fbb1a5554e3428f8d4125307240107";
@@ -18,7 +19,7 @@ class apiController
         echo json_encode([
             "client_ip" => $ip, // The IP address of the requester
             "location" => $response->location->name, // The city of the requester
-            "greeting" => "Hello, $visitor_name!, the temperature is ".$response->current->temp_c." degrees Celcius in ".$response->location->name
+            "greeting" => "Hello, $visitor_name!, the temperature is " . $response->current->temp_c . " degrees Celcius in " . $response->location->name
         ]);
     }
 }
